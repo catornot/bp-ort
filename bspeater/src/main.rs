5911d003f838038b39c1392e51f80134c30cc9af
catornot <41955154+catornot@users.noreply.github.com>
2025-03-27 22:17:57 -0400

    some basic stuff for reading bsps

--- bspeater/src/main.rs
+++ bspeater/src/main.rs
@@ -1,3 +1,201 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code, unused)]
+use std::{
+    fs::File,
+    io::{self, Read, Seek},
+    process::Command,
+};
+
+#[allow(non_camel_case_types, clippy::upper_case_acronyms)]
+enum LumpType {
+    ENTITIES = 0x0000,
+    PLANES = 0x0001,
+    TEXTURE_DATA = 0x0002,
+    VERTICES = 0x0003,
+    LIGHTPROBE_PARENT_INFOS = 0x0004,
+    SHADOW_ENVIRONMENTS = 0x0005,
+    LIGHTPROBE_BSP_NODES = 0x0006,
+    LIGHTPROBE_BSP_REF_IDS = 0x0007,
+    UNUSED_8 = 0x0008,
+    UNUSED_9 = 0x0009,
+    UNUSED_10 = 0x000A,
+    UNUSED_11 = 0x000B,
+    UNUSED_12 = 0x000C,
+    UNUSED_13 = 0x000D,
+    MODELS = 0x000E,
+    UNUSED_15 = 0x000F,
+    UNUSED_16 = 0x0010,
+    UNUSED_17 = 0x0011,
+    UNUSED_18 = 0x0012,
+    UNUSED_19 = 0x0013,
+    UNUSED_20 = 0x0014,
+    UNUSED_21 = 0x0015,
+    UNUSED_22 = 0x0016,
+    UNUSED_23 = 0x0017,
+    ENTITY_PARTITIONS = 0x0018,
+    UNUSED_25 = 0x0019,
+    UNUSED_26 = 0x001A,
+    UNUSED_27 = 0x001B,
+    UNUSED_28 = 0x001C,
+    PHYSICS_COLLIDE = 0x001D,
+    VERTEX_NORMALS = 0x001E,
+    UNUSED_31 = 0x001F,
+    UNUSED_32 = 0x0020,
+    UNUSED_33 = 0x0021,
+    UNUSED_34 = 0x0022,
+    GAME_LUMP = 0x0023,
+    LEAF_WATER_DATA = 0x0024,
+    UNUSED_37 = 0x0025,
+    UNUSED_38 = 0x0026,
+    UNUSED_39 = 0x0027,
+    PAKFILE = 0x0028,
+    UNUSED_41 = 0x0029,
+    CUBEMAPS = 0x002A,
+    TEXTURE_DATA_STRING_DATA = 0x002B,
+    TEXTURE_DATA_STRING_TABLE = 0x002C,
+    UNUSED_45 = 0x002D,
+    UNUSED_46 = 0x002E,
+    UNUSED_47 = 0x002F,
+    UNUSED_48 = 0x0030,
+    UNUSED_49 = 0x0031,
+    UNUSED_50 = 0x0032,
+    UNUSED_51 = 0x0033,
+    UNUSED_52 = 0x0034,
+    UNUSED_53 = 0x0035,
+    WORLD_LIGHTS = 0x0036,
+    WORLD_LIGHT_PARENT_INFOS = 0x0037,
+    UNUSED_56 = 0x0038,
+    UNUSED_57 = 0x0039,
+    UNUSED_58 = 0x003A,
+    UNUSED_59 = 0x003B,
+    UNUSED_60 = 0x003C,
+    UNUSED_61 = 0x003D,
+    UNUSED_62 = 0x003E,
+    UNUSED_63 = 0x003F,
+    UNUSED_64 = 0x0040,
+    UNUSED_65 = 0x0041,
+    TRICOLL_TRIANGLES = 0x0042,
+    UNUSED_67 = 0x0043,
+    TRICOLL_NODES = 0x0044,
+    TRICOLL_HEADERS = 0x0045,
+    UNUSED_70 = 0x0046,
+    VERTEX_UNLIT = 0x0047,
+    VERTEX_LIT_FLAT = 0x0048,
+    VERTEX_LIT_BUMP = 0x0049,
+    VERTEX_UNLIT_TS = 0x004A,
+    VERTEX_BLINN_PHONG = 0x004B,
+    VERTEX_RESERVED_5 = 0x004C,
+    VERTEX_RESERVED_6 = 0x004D,
+    VERTEX_RESERVED_7 = 0x004E,
+    MESH_INDICES = 0x004F,
+    MESHES = 0x0050,
+    MESH_BOUNDS = 0x0051,
+    MATERIAL_SORTS = 0x0052,
+    LIGHTMAP_HEADERS = 0x0053,
+    UNUSED_84 = 0x0054,
+    CM_GRID = 0x0055,
+    CM_GRID_CELLS = 0x0056,
+    CM_GEO_SETS = 0x0057,
+    CM_GEO_SET_BOUNDS = 0x0058,
+    CM_PRIMITIVES = 0x0059,
+    CM_PRIMITIVE_BOUNDS = 0x005A,
+    CM_UNIQUE_CONTENTS = 0x005B,
+    CM_BRUSHES = 0x005C,
+    CM_BRUSH_SIDE_PLANE_OFFSETS = 0x005D,
+    CM_BRUSH_SIDE_PROPERTIES = 0x005E,
+    CM_BRUSH_SIDE_TEXTURE_VECTORS = 0x005F,
+    TRICOLL_BEVEL_STARTS = 0x0060,
+    TRICOLL_BEVEL_INDICES = 0x0061,
+    LIGHTMAP_DATA_SKY = 0x0062,
+    CSM_AABB_NODES = 0x0063,
+    CSM_OBJ_REFERENCES = 0x0064,
+    LIGHTPROBES = 0x0065,
+    STATIC_PROP_LIGHTPROBE_INDICES = 0x0066,
+    LIGHTPROBE_TREE = 0x0067,
+    LIGHTPROBE_REFERENCES = 0x0068,
+    LIGHTMAP_DATA_REAL_TIME_LIGHTS = 0x0069,
+    CELL_BSP_NODES = 0x006A,
+    CELLS = 0x006B,
+    PORTALS = 0x006C,
+    PORTAL_VERTICES = 0x006D,
+    PORTAL_EDGES = 0x006E,
+    PORTAL_VERTEX_EDGES = 0x006F,
+    PORTAL_VERTEX_REFERENCES = 0x0070,
+    PORTAL_EDGE_REFERENCES = 0x0071,
+    PORTAL_EDGE_INTERSECT_AT_EDGE = 0x0072,
+    PORTAL_EDGE_INTERSECT_AT_VERTEX = 0x0073,
+    PORTAL_EDGE_INTERSECT_HEADER = 0x0074,
+    OCCLUSION_MESH_VERTICES = 0x0075,
+    OCCLUSION_MESH_INDICES = 0x0076,
+    CELL_AABB_NODES = 0x0077,
+    OBJ_REFERENCES = 0x0078,
+    OBJ_REFERENCE_BOUNDS = 0x0079,
+    LIGHTMAP_DATA_RTL_PAGE = 0x007A,
+    LEVEL_INFO = 0x007B,
+    SHADOW_MESH_OPAQUE_VERTICES = 0x007C,
+    SHADOW_MESH_ALPHA_VERTICES = 0x007D,
+    SHADOW_MESH_INDICES = 0x007E,
+    SHADOW_MESHES = 0x007F,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Lump {
+    fileofs: i32, // offset into file (bytes)
+    filelen: i32, // length of lump (bytes)
+    version: i32, // lump format version
+    four_cc: i32, // lump ident code}
+}
+
+fn read_i32(reader: &mut dyn Read) -> Result<i32, io::Error> {
+    let mut int = [0; size_of::<i32>()];
+    reader.read_exact(&mut int)?;
+    Ok(i32::from_le_bytes(int))
+}
+
+fn read_lump(reader: &mut dyn Read) -> Result<Lump, io::Error> {
+    Ok(Lump {
+        fileofs: read_i32(reader)?,
+        filelen: read_i32(reader)?,
+        version: read_i32(reader)?,
+        four_cc: read_i32(reader)?,
+    })
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const PATH: &str = "/home/catornot/.local/share/Steam/steamapps/common/Titanfall2/vpk/";
+    const NAME: &str = "englishclient_mp_lf_uma.bsp.pak000_dir.vpk";
+    const UNPACK: &str = "target/vpk";
+
+    Command::new("tf2-vpkunpack")
+        .arg("--exclude")
+        .arg("*")
+        .arg("--include")
+        .arg("maps")
+        .arg(UNPACK)
+        .arg(format!("{PATH}{NAME}"))
+        .spawn()?
+        .wait_with_output()?;
+
+    let mut bsp = File::open(format!("{}/maps/mp_lf_uma.bsp", UNPACK))?;
+
+    let mut file_magic = [0; 4];
+    bsp.read_exact(&mut file_magic)?;
+
+    assert_eq!(&file_magic, b"rBSP");
+
+    // bsp version
+    let mut version = [0; 4];
+    bsp.read_exact(&mut version)?;
+    assert_eq!(version, 37i32.to_le_bytes());
+
+    dbg!(read_lump(&mut bsp)?);
+
+    bsp.seek(io::SeekFrom::Start(
+        (size_of::<i32>() * 2) as u64 + size_of::<Lump>() as u64 * LumpType::CM_BRUSHES as u64,
+    ))?;
+
+    dbg!(read_lump(&mut bsp)?);
+
+    dbg!(read_i32(&mut bsp)?);
+
+    Ok(())
 }
