f9024e24c484353bb6e08b7e0fa317d8d639c12d
catornot <41955154+catornot@users.noreply.github.com>
2025-10-26 21:10:24 -0400

    fix nix hash for rrplug

--- expressions/cargo_lock.nix
+++ expressions/cargo_lock.nix
@@ -2,7 +2,7 @@
 {
   lockFile = ../Cargo.lock;
   outputHashes = {
-    "rrplug-4.1.0" = "sha256-3T/+6mjevcgUzqnO3nRkN1P3Kl7SJvnI7WkbNWuXqNs=";
+    "rrplug-4.2.1" = "sha256-KOhN3J8Sf4gIyNpyY0w53+z26txfRbUv9tbUXvX7o2I=";
     "bevy_gltf_export-0.1.0" = "sha256-eIp8TY96HMpmRpRsryaQzZII5/liCLaScF331GQINfY=";
   };
 }
