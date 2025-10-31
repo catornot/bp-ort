a27134f4caff6973ca03a379d35e761d0e395f82
catornot <41955154+catornot@users.noreply.github.com>
2025-10-31 17:43:45 -0400

    bump version

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
         formatter = native-pkgs.nixfmt-tree;
         packages =
           let
-            version = "0.1.6";
+            version = "0.1.7";
           in
           let
             mkPluginBuildType =
