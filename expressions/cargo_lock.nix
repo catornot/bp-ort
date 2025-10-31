128a723258f0a8dca5a6f8b0c398462f3bd90b01
catornot <41955154+catornot@users.noreply.github.com>
2025-10-31 17:23:33 -0400

    update rrplug

--- expressions/cargo_lock.nix
+++ expressions/cargo_lock.nix
@@ -2,7 +2,7 @@
 {
   lockFile = ../Cargo.lock;
   outputHashes = {
-    "rrplug-4.2.1" = "sha256-KOhN3J8Sf4gIyNpyY0w53+z26txfRbUv9tbUXvX7o2I=";
+    "rrplug-4.2.1" = "sha256-mYn2Z/O1VCryegYjjAb16nTylIvw6TO06EJ/xv9f2hU=";
     "bevy_gltf_export-0.1.0" = "sha256-eIp8TY96HMpmRpRsryaQzZII5/liCLaScF331GQINfY=";
   };
 }
