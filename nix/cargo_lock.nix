87e0dd46b4fc7724e919436c93867dcbc3c4340b
catornot <41955154+catornot@users.noreply.github.com>
2026-08-05 02:40:17 -0400

    fix hash

--- nix/cargo_lock.nix
+++ nix/cargo_lock.nix
@@ -2,7 +2,7 @@
 {
   lockFile = ../Cargo.lock;
   outputHashes = {
-    "rrplug-4.3.0" = "sha256-uzgcjysocmvjau9Xp5K9UNDNpyZMfMMJwgz2KJRU6Tc=";
+    "rrplug-4.3.0" = "sha256-5jYi4vL5/1xPCEc123rMKFSI3teaaM/icMwqzdc0hvQ=";
     "oktree-0.5.1" = "sha256-ppicRhn8qFCSWip60z4qE1f9NS9OsfFo4MaCpv37pzQ=";
   };
 }
